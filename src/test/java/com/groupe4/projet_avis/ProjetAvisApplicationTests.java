8c1ccd8c7de30199aa594f61c008f98a47bd5eb9
JPFormation <karerajp200@gmail.com>
2022-01-20 12:03:36 +0100

    initial commit

---
+++ src/test/java/com/groupe4/projet_avis/ProjetAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe4.projet_avis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
