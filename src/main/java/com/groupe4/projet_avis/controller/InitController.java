15054c67add3a224144b22b03352f1bc4049c493
JPFormation <karerajp200@gmail.com>
2022-01-28 09:20:42 +0100

    controllers

--- src/main/java/com/groupe4/projet_avis/controller/InitController.java
+++ src/main/java/com/groupe4/projet_avis/controller/InitController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.groupe4.projet_avis.entities.Classification;
@@ -23,114 +24,118 @@ import com.groupe4.projet_avis.service.ModerateurService;
 
 @Controller
 public class InitController {
-	private final JoueurService joueurService;
-	private final ModerateurService moderateurService;
-	private final JeuService jeuService;
-	private final AvisService avisService;
-	
-	public InitController(JoueurService joueurService, ModerateurService moderateurService, JeuService jeuService, AvisService avisService) {
-		super();
-		this.joueurService = joueurService;
-		this.moderateurService = moderateurService;
-		this.jeuService = jeuService;
-		this.avisService = avisService;
-	}
-	
-	@PostConstruct
-	private void init() {
-		if(joueurService.getAllJoueurs().isEmpty()) {
-			Joueur j1 = new Joueur();
-			j1.setPseudo("tom cat");
-			j1.setMotDePasse("azerty");
-			j1.setEmail("tom@mail.com");
-			j1.setDateDeNaissance(LocalDate.of(1950, 6, 16));
-			joueurService.saveJoueur(j1);
-			
-			Joueur j2 = new Joueur();
-			j2.setPseudo("jerry mouse");
-			j2.setMotDePasse("qsdfgh");
-			j2.setEmail("jerry@mail.com");
-			j2.setDateDeNaissance(LocalDate.of(1960, 6, 16));
-			joueurService.saveJoueur(j2);
-		}
-		
-		if(moderateurService.getAllModerateurs().isEmpty()) {
-			Moderateur m1 = new Moderateur();
-			m1.setPseudo("mickey mouse");
-			m1.setMotDePasse("poiuyt");
-			m1.setEmail("mickey@mail.com");
-			m1.setNumeroDeTelephone("0612345678");
-			moderateurService.saveModerateur(m1);
-			
-			Moderateur m2 = new Moderateur();
-			m2.setPseudo("donald duck");
-			m2.setMotDePasse("mlkjhg");
-			m2.setEmail("donald@mail.com");
-			m2.setNumeroDeTelephone("0698765432");
-			moderateurService.saveModerateur(m2);
-		}
-		
-		if(jeuService.getAllJeux().isEmpty()) {
-			
-			
-			// créer genres
-			Genre t1 = new Genre();
-			t1.setNom("Shooting");
-			Genre t2 = new Genre();
-			t2.setNom("Racing");
-			
-			// créer classifications
-			Classification c1 = new Classification();
-			c1.setNom("PEGI-12");
-			Classification c2 = new Classification();
-			c2.setNom("PEGI-16");
-			
-			// créer plateformes
-			Plateforme p1 = new Plateforme();
-			p1.setNom("PlayStation");
-			Plateforme p2 = new Plateforme();
-			p2.setNom("Xbox");
-			
-			// créer modelEconomique
-			ModelEconomique m1 = new ModelEconomique();
-			m1.setNom("free to play");
-			ModelEconomique m2 = new ModelEconomique();
-			m2.setNom("pay to play");
-			
-			// créer éditeurs
-			Editeur e1 = new Editeur();
-			e1.setNom("Activision");
-			Editeur e2 = new Editeur();
-			e2.setNom("Sierra");
-			
-			Jeu g1 = new Jeu();
-			g1.setNom("Medal of Honour");
-			g1.setEditeur(e1);
-			g1.setGenre(t1);
-			g1.setModeleEconomique(m1);
-			g1.setClassification(c2);
-			Set<Plateforme> ps1 = new HashSet<>();
-			ps1.add(p1);
-			ps1.add(p2);
-			g1.setPlateformes(ps1);
-			jeuService.saveJeu(g1);
-			
-			Jeu g2 = new Jeu();
-			g2.setNom("Need for Speed");
-			g2.setEditeur(e2);
-			g2.setGenre(t2);
-			g2.setModeleEconomique(m2);
-			g2.setClassification(c1);
-			Set<Plateforme> ps2 = new HashSet<>();
-			ps1.add(p2);
-			g2.setPlateformes(ps2);
-			jeuService.saveJeu(g2);
-		}
-		
-		if(avisService.getAllAvis().isEmpty()) {
-			
-		}
-	}
+	@Autowired
+	private  JoueurService joueurService;
+	@Autowired
+	private  ModerateurService moderateurService;
+	@Autowired
+	private  JeuService jeuService;
+	@Autowired
+	private  AvisService avisService;
+//	
+//	public InitController(JoueurService joueurService, ModerateurService moderateurService, JeuService jeuService, AvisService avisService) {
+//		super();
+//		this.joueurService = joueurService;
+//		this.moderateurService = moderateurService;
+//		this.jeuService = jeuService;
+//		this.avisService = avisService;
+//	}
+//	
+//	@PostConstruct
+//	private void init() {
+//		if(joueurService.getAllJoueurs().isEmpty()) {
+//			Joueur j1 = new Joueur();
+//			j1.setPseudo("tom cat");
+//			j1.setMotDePasse("azerty");
+//			j1.setEmail("tom@mail.com");
+//			j1.setDateDeNaissance(LocalDate.of(1950, 6, 16));
+//			joueurService.saveJoueur(j1);
+//			
+//			Joueur j2 = new Joueur();
+//			j2.setPseudo("jerry mouse");
+//			j2.setMotDePasse("qsdfgh");
+//			j2.setEmail("jerry@mail.com");
+//			j2.setDateDeNaissance(LocalDate.of(1960, 6, 16));
+//			joueurService.saveJoueur(j2);
+//		}
+//		
+//		if(moderateurService.getAllModerateurs().isEmpty()) {
+//			Moderateur m1 = new Moderateur();
+//			m1.setPseudo("mickey mouse");
+//			m1.setMotDePasse("poiuyt");
+//			m1.setEmail("mickey@mail.com");
+//			m1.setNumeroDeTelephone("0612345678");
+//			moderateurService.saveModerateur(m1);
+//			
+//			Moderateur m2 = new Moderateur();
+//			m2.setPseudo("donald duck");
+//			m2.setMotDePasse("mlkjhg");
+//			m2.setEmail("donald@mail.com");
+//			m2.setNumeroDeTelephone("0698765432");
+//			moderateurService.saveModerateur(m2);
+//		}
+//		
+//		if(jeuService.getAllJeux().isEmpty()) {
+//			
+//			
+//			// créer genres
+//			Genre t1 = new Genre();
+//			t1.setNom("Shooting");
+//			Genre t2 = new Genre();
+//			t2.setNom("Racing");
+//			
+//			// créer classifications
+//			Classification c1 = new Classification();
+//			c1.setNom("PEGI-12");
+//			Classification c2 = new Classification();
+//			c2.setNom("PEGI-16");
+//			
+//			// créer plateformes
+//			Plateforme p1 = new Plateforme();
+//			p1.setNom("PlayStation");
+//			Plateforme p2 = new Plateforme();
+//			p2.setNom("Xbox");
+//			
+//			// créer modelEconomique
+//			ModelEconomique m1 = new ModelEconomique();
+//			m1.setNom("free to play");
+//			ModelEconomique m2 = new ModelEconomique();
+//			m2.setNom("pay to play");
+//			
+//			// créer éditeurs
+//			Editeur e1 = new Editeur();
+//			e1.setNom("Activision");
+//			Editeur e2 = new Editeur();
+//			e2.setNom("Sierra");
+//			
+//			Jeu g1 = new Jeu();
+//			g1.setNom("Medal of Honour");
+//			g1.setEditeur(e1);
+//			g1.setGenre(t1);
+//			g1.setModeleEconomique(m1);
+//			g1.setClassification(c2);
+//			Set<Plateforme> ps1 = new HashSet<>();
+//			ps1.add(p1);
+//			ps1.add(p2);
+//			g1.setPlateformes(ps1);
+//			jeuService.saveJeu(g1);
+//			
+//			Jeu g2 = new Jeu();
+//			g2.setNom("Need for Speed");
+//			g2.setEditeur(e2);
+//			g2.setGenre(t2);
+//			g2.setModeleEconomique(m2);
+//			g2.setClassification(c1);
+//			Set<Plateforme> ps2 = new HashSet<>();
+//			ps1.add(p2);
+//			g2.setPlateformes(ps2);
+//			jeuService.saveJeu(g2);
+//		}
+//		
+//		if(avisService.getAllAvis().isEmpty()) {
+//			
+//		}
+//	}
 	
 	
 }
